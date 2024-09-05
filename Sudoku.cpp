f035893a0ae06d3e523b7e667453b310e1fb050d
Cat code <jahangirov_mansur@protonmail.com>
2024-09-05 22:58:44 +0300

    Добавил пару функций bool

--- Sudoku.cpp
+++ Sudoku.cpp
@@ -5,22 +5,27 @@
 using namespace std;
 
 #проверка соответсвия условиям
-int return_method (int ss, int su) {
-    for (int x = 1; i < 10; ++i) {
-        for (int i = 0; i < 9; ++i) {
-        for (int j = 0; j < 9; ++j) {
-            cout << sudoku[i][j] << " ";
+bool SolveSudoku(int su[9][9]) {
+    for (int x=0; x < 9, x++) {
+        for (int y = 0; y < 9; y++) {
+            if (su[x][y] == 0)
         }
-    }
-
     }
 }
 
+bool Correct {
+    for (int i = 0; i < 9; i++) {
+        if (grid[row][i] == num || grid[i][col] == num || 
+            grid[row - row % 3 + i / 3][col - col % 3 + i % 3] == num) {
+            return false;
+        }
+    }
+    return true;
+}
 
 int main()
 {
-    int su[9][9]; #нерешенный
-    int ss[9][9]; #решенный судоку
+    int su[9][9];
     cout << " Введите ваш судоку, вместо пустой строки введите 0:" << endl;
     for (int i = 0; i < 9; ++i) {
         for (int j = 0; j < 9; ++j) {
@@ -39,9 +44,9 @@ int main()
     for (int i = 0; i < 9; ++i) {
         for (int j = 0; j < 9; ++j) {
             if (su[i][j] != 0)
-            ss[i][j] = su[i][j];
+            su[i][j] = su[i][j];
             else
-            return_method (int ss[i][j])
+            SolveSudoku (int su[9][9]);
         }
     }
 
